bfa1d84efcd6129483d8c50677eb7361886469bb
Bill Cheng <so89898@gmail.com>
2014-09-01 01:13:09 +0800

    Initial Commit

---
+++ TwitterBlur/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TwitterBlur
+//
+//  Created by Bill Cheng on 14-9-1.
+//  Copyright (c) 2014年 R3 Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
